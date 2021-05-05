e8c3b73e35df0a92800f151f1e9da81cb633cd6d
Darfreed <schwanmarek@gmail.com>
2021-05-05 19:22:19 +0200

    detail

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('templates/books', views.book, name="books"),
-    path('templates/authors', views.author, name="authors"),
+    path('books', views.BookView.as_view(), name="books"),
+    path('authors', views.AuthorView.as_view(), name="authors"),
+    path('book_detail/<int:pk>', views.BookDetailView.as_view(), name="book_detail"),
+    path('author_detail/<int:pk>', views.AuthorDetailView.as_view(), name="author_detail"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,35 +1,49 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 from .models import Author, Book, Genre
 
-def index(request):
 
+def index(request):
     books = Book.objects.order_by('-rate')[:4]
     context = {
         'books': books
     }
-
     return render(request, '../templates/index.html', context=context)
 
-def book(request):
 
-    num_books = Book.objects.all().count()
-    books = Book.objects.all()
-    context = {
-        'num_books': num_books,
-        'books': books
-    }
+class BookView(TemplateView):
+    template_name = "templates/books.html"
 
-    return render(request, '../templates/books.html', context=context)
-
-def author(request):
-
-    num_authors = Author.objects.all().count()
-    authors = Author.objects.all()
-    context = {
-        'num_authors': num_authors,
-        'authors': authors
-    }
-
-    return render(request, '../templates/authors.html', context=context)
+    def get(self, request, *args, **kwargs):
+        num_books = Book.objects.all().count()
+        books = Book.objects.all()
+        context = {
+            'num_books': num_books,
+            'books': books
+        }
+        return render(request, '../templates/books.html', context=context)
 
 
+class AuthorView(TemplateView):
+    template_name = "templates/authors.html"
+
+    def get(self, request, *args, **kwargs):
+        num_authors = Author.objects.all().count()
+        authors = Author.objects.all()
+        context = {
+            'num_authors': num_authors,
+            'authors': authors
+        }
+
+        return render(request, '../templates/authors.html', context=context)
+
+
+class BookDetailView(TemplateView):
+    model = Book
+    context_object_name = 'book'
+    template_name = '../templates/books_detail.html'
+
+class AuthorDetailView(TemplateView):
+    model = Author
+    context_object_name = 'author'
+    template_name = '../templates/authors_detail.html'
