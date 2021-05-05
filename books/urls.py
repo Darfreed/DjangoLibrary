19db3eb47302d4211415ede7a13416202a3b257d
Darfreed <schwanmarek@gmail.com>
2021-05-05 10:49:38 +0200

    fourth_commit

--- books/urls.py
+++ books/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('templates/books', views.book, name="books"),
+    path('templates/authors', views.author, name="authors"),
 ]
--- books/views.py
+++ books/views.py
@@ -3,13 +3,33 @@ from .models import Author, Book, Genre
 
 def index(request):
 
+    books = Book.objects.order_by('-rate')[:4]
+    context = {
+        'books': books
+    }
+
+    return render(request, '../templates/index.html', context=context)
+
+def book(request):
+
     num_books = Book.objects.all().count()
-    books = Book.objects.order_by('-rate')[:6]
+    books = Book.objects.all()
     context = {
         'num_books': num_books,
         'books': books
     }
 
-    return render(request, '../templates/index.html', context=context)
+    return render(request, '../templates/books.html', context=context)
+
+def author(request):
+
+    num_authors = Author.objects.all().count()
+    authors = Author.objects.all()
+    context = {
+        'num_authors': num_authors,
+        'authors': authors
+    }
+
+    return render(request, '../templates/authors.html', context=context)
 
 
